6aed591940ec7225e65d0a86a3f11490495cabf5
Mohammed habeeb ashraf <habeebashraf467@gmail.com>
2025-11-25 16:13:34 +0530

    Update MongoDB connection to use environment variable

--- config/mongooseconnection.js
+++ config/mongooseconnection.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
 const config = require('config');
 
-mongoose.connect
-(`${config.get('MONGODB_URI')}/instagram`)
-.then(function(){
-    console.log("connected");
-})
-.catch(function(err){
-    console.log(err);
-});
+// mongoose.connect
+// (`${config.get('MONGODB_URI')}/instagram`)
+// .then(function(){
+//     console.log("connected");
+// })
+// .catch(function(err){
+//     console.log(err);
+// });
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("Connected to MongoDB"))
+.catch(err => console.log(err));
 
 module.exports = mongoose.connection;
 
