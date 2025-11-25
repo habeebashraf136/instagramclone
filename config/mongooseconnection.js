6dc655c52fbdb35ee48f59f1536798d333648217
Mohammed habeeb ashraf <habeebashraf467@gmail.com>
2025-11-25 17:41:21 +0530

    Update MongoDB connection URI environment variable

--- config/mongooseconnection.js
+++ config/mongooseconnection.js
@@ -10,7 +10,7 @@ const config = require('config');
 //     console.log(err);
 // });
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log("Connected to MongoDB"))
 .catch(err => console.log(err));
 
