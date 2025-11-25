1fada924a8ed056b0c4913fa94d0ac8ed0cd6041
Mohammed habeeb ashraf <habeebashraf467@gmail.com>
2025-11-25 17:50:04 +0530

    Refactor MongoDB connection to use environment variable

--- config/mongooseconnection.js
+++ config/mongooseconnection.js
@@ -1,15 +1,6 @@
 const mongoose = require('mongoose');
 const config = require('config');
 
-// mongoose.connect
-// (`${config.get('MONGODB_URI')}/instagram`)
-// .then(function(){
-//     console.log("connected");
-// })
-// .catch(function(err){
-//     console.log(err);
-// });
-
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log("Connected to MongoDB"))
 .catch(err => console.log(err));
